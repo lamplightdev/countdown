ee8385b46088bebd7cbeae57a22be196ca2c89ae
Chris Haynes (mac) <chris.haynes@gmail.com>
2016-08-09 21:09:37 +0100

    cleanup

--- waypoint/index.js
+++ waypoint/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const session = require('express-session');
 const compression = require('compression');
 const bodyParser = require('body-parser');
 
@@ -9,6 +10,16 @@ const createVirtualConsole = require('jsdom').createVirtualConsole;
 
 
 const app = express();
+
+app.use(session({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: true,
+  cookie: {
+    secure: false,
+  },
+}));
+
 app.use(compression());
 app.use(express.static('./static'));
 
@@ -17,17 +28,82 @@ app.use(bodyParser.urlencoded({
   extended: true,
 }));
 
-app.get('/:pageName?', (req, res) => {
+const defined = (...vars) => vars.every(varName => typeof varName !== 'undefined');
+
+app.get('/:pageName?', (req, res, next) => {
+  const validPages = [
+    'list',
+    'account',
+  ];
+
   const pageName = req.params.pageName || 'list';
 
-  const html = fs.readFileSync('./index.html');
-  const doc = jsdom(html.toString().replace(/##pageName##/g, pageName), {
-    virtualConsole: createVirtualConsole().sendTo(console),
-  });
+  if (validPages.indexOf(pageName) > -1) {
+    const html = fs.readFileSync('./index.html');
 
-  doc.defaultView.onload = () => {
-    res.send(serializeDocument(doc));
-  };
+    if (!req.session.items) {
+      req.session.items = [];
+    }
+
+    const initParams = {
+      items: req.session.items,
+    };
+
+    const htmlString = html
+      .toString()
+      .replace(/##pageName##/g, JSON.stringify(pageName))
+      .replace(/##initParams##/g, JSON.stringify(initParams));
+
+    const doc = jsdom(htmlString, {
+      virtualConsole: createVirtualConsole().sendTo(console),
+    });
+
+    doc.defaultView.onload = () => {
+      res.send(serializeDocument(doc));
+    };
+  } else {
+    next();
+  }
+});
+
+app.post('/add', (req, res) => {
+  if (defined(req.body.id, req.body.title, req.body.latitude, req.body.longitude)) {
+    req.session.items.push({
+      id: req.body.id,
+      title: req.body.title,
+      latitude: req.body.latitude,
+      longitude: req.body.longitude,
+    });
+
+    res.json(true);
+  } else {
+    res.json(false);
+  }
+});
+
+app.post('/edit/:id', (req, res) => {
+  if (defined(req.params.id, req.body.title, req.body.latitude, req.body.longitude)) {
+    req.session.items[req.params.id] = {
+      id: req.body.id,
+      title: req.body.title,
+      latitude: req.body.latitude,
+      longitude: req.body.longitude,
+    };
+
+    res.json(true);
+  } else {
+    res.json(false);
+  }
+});
+
+app.post('/remove/:id', (req, res) => {
+  if (defined(req.params.id)) {
+    req.session.items.splice(req.params.id, 1);
+
+    res.json(true);
+  } else {
+    res.json(false);
+  }
 });
 
 app.listen(5000, () => {
